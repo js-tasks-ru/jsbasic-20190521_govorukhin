6f935a4a83325b49fe52863319c1280d5ceb5629
Stanislav Govorukhin <signups@discipline.work>
2019-05-25 19:39:04 +0300

    fixed returns

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -3,32 +3,34 @@
 /**
  * Эту функцию трогать не нужно
  */
-function print (text) {
-    alert(text);
+function print(text) {
+  alert(text);
 }
 
 /**
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid (name) {
+function isValid(name) {
   if (name.length >= 4) {
-    if (name.indexOf(" ") != -1) {
-      return !isValid;
+    if (~name.indexOf(" ")) {
+      return false;
     } else {
-      return isValid;
-  }
+      return true;
+    }
+  } else {
+    return false;
   }
 }
 
-function sayHello () {
-    let userName = prompt('Введите ваше имя');
+function sayHello() {
+  let userName = prompt('Введите ваше имя');
 
-    if (isValid(userName)) {
-        print('Welcome back, ' + userName + '!');
-    } else {
-        print('Некорректное имя');
-    }
+  if (isValid(userName)) {
+    print('Welcome back, ' + userName + '!');
+  } else {
+    print('Некорректное имя');
+  }
 }
 
 sayHello();
