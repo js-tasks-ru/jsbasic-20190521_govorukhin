e66f14e7c9302cc66ea2268a380294b59080f470
Stanislav Govorukhin <signups@discipline.work>
2019-05-25 16:35:09 +0300

    Решение первой задачи первого модуля (возведение в степень)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,23 @@
  * @param {number} n index
  * @returns {number}
  */
-function pow (m, n) {
-
-}
+function pow(x, n) {
+    var powResult = x;
+  
+    for (var i = 1; i < n; i++) {
+      powResult *= x;
+    }
+  
+    return powResult;
+  }
+  
+  var x = prompt("Какое число возводим в степень?", '');
+  var n = prompt("В какую степень возводим это число?", '');
+  
+  if (n <= 1) {
+    alert('Так нельзя. Введите степень строго больше 1');
+  } 
+  else 
+  {
+    alert(pow(x, n));
+  }
