9cbf0581fef47260f617e5cb5c3889739a4de4b8
Stanislav Govorukhin <signups@discipline.work>
2019-06-05 01:24:21 +0300

    Решил создавать объект result изначально пустым, чтобы потом посчитать min и max и записать их внутрь этого объекта

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,24 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
 function getMinMax(str) {
+    
+  let strConverter = str.replace(/,/g, ' ').split(/\s/);
+  
+  let result = {};
+  let minVar = 0;
+  let maxVar = 0;
 
+  for (let n in strConverter) {
+    if (minVar > +strConverter[n]) {
+        minVar = +strConverter[n];
+    };
+    if (maxVar < +strConverter[n]) {
+        maxVar = +strConverter[n];
+    }
+    result.min = minVar;
+    result.max = maxVar;
+  }
+  return result;
 }
-
